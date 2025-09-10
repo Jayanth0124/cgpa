27313dc3f32abe856c3094863d0065f3d38dc292
jayanth0124 <jayanthdonavalli0124@gmail.com>
2025-09-10 16:36:12 +0530

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // --- ADD THIS LINE ---
+  // This line is essential for Netlify deployment
   base: './', 
 })
